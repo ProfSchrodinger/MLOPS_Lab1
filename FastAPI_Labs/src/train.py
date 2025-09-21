343e4977636c8fed7a36a18894729a30e6659785
ProfSchrodinger <sabarimathavan24@gmail.com>
2025-09-20 22:07:06 -0400

    diabetes model creation

--- FastAPI_Labs/src/train.py
+++ FastAPI_Labs/src/train.py
@@ -1,21 +1,23 @@
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 import joblib
 from data import load_data, split_data
 
+# def fit_model(X_train, y_train):
+#     """
+#     Train a Decision Tree Classifier and save the model to a file.
+#     Args:
+#         X_train (numpy.ndarray): Training features.
+#         y_train (numpy.ndarray): Training target values.
+#     """
+#     dt_classifier = DecisionTreeClassifier(max_depth=3, random_state=12)
+#     dt_classifier.fit(X_train, y_train)
+#     joblib.dump(dt_classifier, "../model/iris_model.pkl")
+
 def fit_model(X_train, y_train):
-    """
-    Train a Decision Tree Classifier and save the model to a file.
-    Args:
-        X_train (numpy.ndarray): Training features.
-        y_train (numpy.ndarray): Training target values.
-    """
-    # dt_classifier = DecisionTreeClassifier(max_depth=3, random_state=12)
-    model = RandomForestClassifier(n_estimators=100, max_depth=3, random_state=42)
-    # dt_classifier.fit(X_train, y_train)
+    model = LinearRegression()
     model.fit(X_train, y_train)
-    # joblib.dump(dt_classifier, "../model/iris_model.pkl")
-    joblib.dump(model, "../model/rf_iris_model.pkl")
+    joblib.dump(model, "../model/diabetes_model.pkl")
 
 if __name__ == "__main__":
     X, y = load_data()
